aef9e7ce958a60d5a306327fe348f18b0675366d
salesmiguelz <miguelsales02@gmail.com>
2025-05-03 14:50:51 -0300

    feat: added styled components

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,4 +1,4 @@
-import styled, {css} from 'styled-components'
+import styled from 'styled-components'
 
 export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
@@ -6,18 +6,14 @@ interface ButtonContainerProps{
     variant: ButtonVariant
 }
 
-const buttonVariants = {
-    primary: 'purple',
-    secondary: 'orange',
-    danger: 'red',
-    success: 'green'
-}
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
     height: 40px;
-    ${props => {
-        return css`background-color: ${buttonVariants[props.variant]}` 
-    }}
+    border-radius: 4px;
+    border: 0;
+    margin: 8px;
 
+    background-color: ${props => props.theme['green-500']};
+    color: ${props => props.theme.white}
 `
